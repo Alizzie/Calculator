9d8e65551537a4f138dd91c5c236bdae705530d6
Alizzie <79795653+Alizzie@users.noreply.github.com>
2021-08-25 15:10:54 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -4,17 +4,18 @@ const toggler = document.getElementById("slider");
 const display = document.getElementsByClassName("displayText")[0];
 const togNum = Array.from(document.getElementsByTagName("Span"));
 
-togNum.forEach(num => num.addEventListener("click", (e) => {
+togNum.forEach(num => num.addEventListener("touchstart", (e) => {
   console.log(e.path[0].innerText);
   toggler.value = Number(e.path[0].innerText) - 1;
+  alert("gak");
   console.log("Toggler Value: " + toggler.value);
   }
 ));
 
-toggler.addEventListener("click", () => {
+/* toggler.addEventListener("click", () => {
   console.log("Toggler Value: " + toggler.value);
   checkTheme(toggler.value);
-})
+})*/
 
 //<<<<---------------------- Session Storage -------------------------->>>>>>
 //Situation: Reloading the page
